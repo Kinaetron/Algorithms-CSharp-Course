b0c331fbb993e12a88a577cec6d75e1f35eba407
Darren <darren.daley@hotmail.co.uk>
2020-02-19 20:03:06 +0000

    Dictionary work

---
+++ Algorithms DataStruct-Lib/ChainHashSet.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_DataStruct_Lib
+{
+    public class ChainHashSet<TKey, TValue>
+    {
+        private SequentialSearchST<TKey, TValue> []chains;
+
+        private const int defaultCapacity = 4;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        public ChainHashSet():this(Prime.MinPrime)
+        {
+
+        }
+
+        public ChainHashSet(int capacity)
+        {
+            Capacity = capacity;
+            chains = new SequentialSearchST<TKey, TValue>[capacity];
+            for (int i = 0; i < Capacity; i++)
+            {
+                chains[i] = new SequentialSearchST<TKey, TValue>();
+            }
+        }
+
+        private int Hash(TKey key) {
+            return (key.GetHashCode() & 0x7fffffff) % Capacity;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if(key == null) {
+                throw new ArgumentNullException("Key is not allowed to be null");
+            }
+
+            int index = Hash(key);
+            if(chains[index].TryGet(key, out TValue val)) {
+                return val;
+            }
+
+            throw new ArgumentException("Key was not found.");
+        }
+
+        public bool Cotains(TKey key)
+        {
+            if(key == null) {
+                throw new ArgumentNullException("key is not allowed to be null");
+            }
+
+            int index = Hash(key);
+            return chains[index].TryGet(key, out TValue _);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null) {
+                throw new ArgumentNullException("key is not allowed to be null");
+            }
+
+            int index = Hash(key);
+            if(chains[index].Contains(key))
+            {
+                Count--;
+                chains[index].Remove(key);
+
+                if(Capacity > defaultCapacity && Count <= 2*Capacity) {
+                    Resize(Prime.ReducePrime(Capacity));
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null) {
+                throw new ArgumentNullException("key is not allowed to be null");
+            }
+
+            if(value == null)
+            {
+                Remove(key);
+                return;
+            }
+
+            if (Count >= 10 * Capacity) {
+                Resize(Prime.ExpandPrime(Capacity));
+            }
+
+            int i = Hash(key);
+            if(!chains[i].Contains(key)) {
+                Count++;
+            }
+
+            chains[i].Add(key, value);
+        }
+
+        private void Resize(int chains)
+        {
+            var temp = new ChainHashSet<TKey, TValue>(chains);
+            for (int i = 0; i < Capacity; i++)
+            {
+                foreach (TKey key in this.chains[i].Keys())
+                {
+                    if(this.chains[i].TryGet(key, out TValue val))
+                    {
+                        temp.Add(key, val);
+                    }
+                }
+            }
+
+            Capacity = temp.Capacity;
+            Count = temp.Count;
+            this.chains = temp.chains;
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            var queue = new LinkedQueue<TKey>();
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                foreach (TKey key in chains[i].Keys()){
+                    queue.Enqueue(key);
+                }
+            }
+
+            return queue;
+        }
+    }
+}
---
+++ Algorithms DataStruct-Lib/LinearProbingHashSet.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_DataStruct_Lib
+{
+    public class LinearProbingHashSet<TKey, TValue>
+    {
+        private const int defaultCapacity = 4;
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+        private TKey[] keys;
+        private TValue[] values;
+
+        public LinearProbingHashSet():this(defaultCapacity)
+        {
+
+        }
+
+        public LinearProbingHashSet(int capacity)
+        {
+            Capacity = capacity;
+            keys = new TKey[capacity];
+            values = new TValue[capacity];
+        }
+
+        private int Hash(TKey key)
+        {
+            return (key.GetHashCode() & 0x7fffffff) % Capacity;
+        }
+
+        public bool Contains(TKey key)
+        {
+            if(key == null) {
+                throw new ArgumentNullException("Key is not allowed to be null");
+            }
+
+            for (int i = Hash(key); keys[i] != null; i = (i+1)%Capacity)
+            {
+                if(keys[i].Equals(key)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public TValue Get(TKey key)
+        {
+            if(key == null) {
+                throw new ArgumentNullException("Key isn's allowed to be null");
+            }
+
+            for (int i = Hash(key); keys[i] != null; i = (i + 1) % Capacity)
+            {
+                if(keys.Equals(key)) {
+                    return values[i];
+                }
+            }
+
+            throw new ArgumentException("Key was not found");
+        }
+
+        public bool TryGet(TKey key, out int index)
+        {
+            if (key == null) {
+                throw new ArgumentNullException("Key isn's allowed to be null");
+            }
+
+            for (int i = Hash(key); keys[i] != null; i = (i + 1) % Capacity)
+            {
+                if (keys.Equals(key)) {
+                    index = i;
+                    return true;
+                }
+            }
+
+            index = -1;
+            return false;
+        }
+
+        public void Remove(TKey key)
+        {
+            if (key == null) {
+                throw new ArgumentNullException("Key isn's allowed to be null");
+            }
+
+            if(!TryGet(key, out int index)) {
+                return;
+            }
+
+            keys[index] = default(TKey);
+            values[index] = default(TValue);
+
+            index = (index + 1) % Capacity;
+
+            while (keys[index] != null)
+            {
+                TKey keyToRehash = keys[index];
+                TValue valToRehash = values[index];
+
+                keys[index] = default(TKey);
+                values[index] = default(TValue);
+
+                Count--;
+
+                Add(keyToRehash, valToRehash);
+
+                index = (index + 1) % Capacity;
+            }
+
+            Count--;
+
+            if(Count > 0 && Count <= Capacity / 8) {
+                Resize(Capacity / 2);
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null) {
+                throw new ArgumentNullException("Key isn's allowed to be null");
+            }
+
+            if(value == null)
+            {
+                Remove(key);
+                return;
+            }
+
+            if(Count >= Capacity / 2) {
+                Resize(2 * Capacity);
+            }
+
+            int i;
+            for (i = Hash(key); keys[i] != null; i = (i + 1) % Capacity)
+            {
+                if(keys.Equals(key))
+                {
+                    values[i] = value;
+                    return;
+                }
+            }
+
+            keys[i] = key;
+            values[i] = value;
+        }
+
+        private void Resize(int capacity)
+        {
+            var temp = new LinearProbingHashSet<TKey, TValue>(capacity);
+
+            for (int i = 0; i < Capacity; i++)
+            {
+                if(keys[i] != null) {
+                    temp.Add(keys[i], values[i]);
+                }
+            }
+
+            keys = temp.keys;
+            values = temp.values;
+
+            Capacity = temp.Capacity;
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            var q = new Queue<TKey>();
+            for (int i = 0; i < Capacity; i++)
+            {
+                if(keys[i] != null) {
+                    q.Enqueue(keys[i]);
+                }
+            }
+
+            return q;
+        }
+    }
+}
